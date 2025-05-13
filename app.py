32a1554d74f646b17b6ce2f815359bbbd45cc3e3
watanabe kota <w.kouta1219@gmail.com>
2025-05-13 14:06:30 +0900

    first commit

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import json
 import base64
 from flask import Flask, request
 from datetime import datetime
+from pytz import timezone
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from io import StO
 
 app = Flask(__name__)
 
-# スプレッドシート名（任意）ringI
+# スプレッドシート名（任意）
 SPREADSHEET_NAME = "click_log"
 
 # 環境変数から credentials.json を再現
@@ -25,7 +25,7 @@ gc = gspread.authorize(credentials)
 def log_click():
     data = request.get_json()
     event = data.get("button", "unknown")
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.now(timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
 
     sh = gc.open(SPREADSHEET_NAME)
 
